84da42730d6c85ad536e59c13a76e889ea306833
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-11-11 14:14:31 +0100

    update configuration

--- SimpleWebApp/public/env-config.js
+++
@@ -1,4 +0,0 @@
-// used as 'environment' variables
-window.ASSISTANT_INTEGRATION_ID="fb6dac0e-7249-4461-bf19-798d3b68505b"
-window.ASSISTANT_REGION="us-south"
-window.ASSISTANT_SERVICE_INSTANCE_ID="07061aa9-b6d8-427d-af36-947da5f8a12e"
